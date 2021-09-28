a40d299904332783daa6224807ccef9dab15f870
James Bridge <naetharu@gmail.com>
2021-09-28 22:30:46 +0100

    reworked the CSS

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,65 @@
+// Global Variables
+let bill = 0;
+let people = 1;
+let tip = 5;
+
+const updateResults = () => {
+  let tipPerPerson = ((bill / 100) * tip) / people;
+  let totalPerPerson = bill / people;
+  document.getElementById("per-result").innerText =
+    "$" + tipPerPerson.toFixed(2);
+  document.getElementById("total-result").innerText =
+    "$" + totalPerPerson.toFixed(2);
+};
+
 const updateBill = () => {
-  console.log("update bill");
+  bill = document.getElementById("bill").value;
+  if (bill < 0) {
+    bill = 0;
+  }
+  updateResults();
+};
+
+// Add event listeners to each tip button
+const chooseTip = () => {};
+const tips = ["5", "10", "15", "25", "50"];
+tips.forEach((amount) => {
+  document.getElementById(amount).addEventListener("click", () => {
+    tip = amount;
+    updateResults();
+  });
+});
+
+// Custom Tip
+const customTip = () => {
+  let input = document.getElementById("custom-tip").value;
+  let number = parseInt(input);
+
+  if (isNaN(number)) {
+    tip = 0;
+  } else {
+    tip = number.toFixed(2);
+  }
+
+  updateResults();
 };
 
 const updatePeople = () => {
-  console.log("update people");
+  people = document.getElementById("people").value;
+  if (people < 1) {
+    people = 1;
+  }
+  updateResults();
+};
+
+const reset = () => {
+  bill = 0;
+  people = 1;
+  tip = 5;
+
+  document.getElementById("bill").value = "";
+  document.getElementById("people").value = "";
+  document.getElementById("custom-tip").value = "";
+
+  updateResults();
 };
